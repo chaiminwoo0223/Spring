807a5c4cbdf81bfc73ec16fd6e0efc17730ba812
Choi, Minwoo <chaiminwoo0223@gmail.com>
2024-11-05 23:02:14 +0900

    feat: 등록 기능 추가하기

--- spring-introduction/src/main/java/study/spring/introduction/controller/MemberController.java
+++ spring-introduction/src/main/java/study/spring/introduction/controller/MemberController.java
@@ -2,10 +2,27 @@ package study.spring.introduction.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import study.spring.introduction.domain.Member;
+import study.spring.introduction.domain.MemberForm;
 import study.spring.introduction.service.MemberService;
 
 @Controller // Spring Container에 Spring Bean 등록(@Component)
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+        return "redirect:/";
+    }
 }
---
+++ spring-introduction/src/main/java/study/spring/introduction/domain/MemberForm.java
@@ -0,0 +1,10 @@
+package study.spring.introduction.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberForm {
+    private String name;
+}
