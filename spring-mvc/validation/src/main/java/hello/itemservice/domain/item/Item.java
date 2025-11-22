26ff013c86826497dea4fb2fa092b8ac68bd73db
Choi, Minwoo <chaiminwoo0223@gmail.com>
2025-11-22 20:43:36 +0900

    검증2 - Bean Validation

--- spring-mvc/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ spring-mvc/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,14 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
-
 @Data
 public class Item {
-
     private Long id;
+
     private String itemName;
+
     private Integer price;
+
     private Integer quantity;
 
     public Item() {
