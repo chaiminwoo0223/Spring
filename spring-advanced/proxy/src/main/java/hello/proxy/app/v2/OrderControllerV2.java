6fdedb920402709910bff0b959ac9102c8b77cbe
Choi, Minwoo <chaiminwoo0223@gmail.com>
2025-12-25 20:30:52 +0900

    프록시 패턴

---
+++ spring-advanced/proxy/src/main/java/hello/proxy/app/v2/OrderControllerV2.java
@@ -0,0 +1,26 @@
+package hello.proxy.app.v2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequestMapping("/v2")
+@ResponseBody
+public class OrderControllerV2 {
+    private final OrderServiceV2 orderService;
+
+    public OrderControllerV2(OrderServiceV2 orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/request")
+    public String request(String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+
+    @GetMapping("/no-log")
+    public String noLog() {
+        return "ok";
+    }
+}
---
+++ spring-advanced/proxy/src/main/java/hello/proxy/app/v3/OrderControllerV3.java
@@ -0,0 +1,22 @@
+package hello.proxy.app.v3;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/v3")
+@RequiredArgsConstructor
+public class OrderControllerV3 {
+    private final OrderServiceV3 orderService;
+
+    @GetMapping("/request")
+    public String request(String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+
+    @GetMapping("/no-log")
+    public String noLog() {
+        return "ok";
+    }
+}
