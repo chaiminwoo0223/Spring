c9e7a3de966033922bb063bb86c81fc3e19668ad
Choi, Minwoo <chaiminwoo0223@gmail.com>
2025-12-25 13:40:28 +0900

    ThreadLocal

--- spring-advanced/advanced/src/test/java/hello/advanced/trace/threadlocal/ThreadLocalServiceTest.java
+++ spring-advanced/advanced/src/test/java/hello/advanced/trace/threadlocal/ThreadLocalServiceTest.java
@@ -1,12 +1,12 @@
 package hello.advanced.trace.threadlocal;
 
-import hello.advanced.trace.threadlocal.code.FieldService;
+import hello.advanced.trace.threadlocal.code.ThreadLocalService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
-public class FieldServiceTest {
-    private FieldService service = new FieldService();
+public class ThreadLocalServiceTest {
+    private ThreadLocalService service = new ThreadLocalService();
 
     @Test
     void field() {
