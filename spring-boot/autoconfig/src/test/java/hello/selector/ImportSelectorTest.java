298522b2dc081adc4f768d3e8216e7163adec6a9
Choi, Minwoo <chaiminwoo0223@gmail.com>
2025-12-29 23:16:03 +0900

    스프링 부트의 자동 구성 2

---
+++ spring-boot/autoconfig/src/test/java/hello/selector/ImportSelectorTest.java
@@ -0,0 +1,33 @@
+package hello.selector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ImportSelectorTest {
+
+    @Test
+    void staticConfig() {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(StaticConfig.class);
+        HelloBean bean = context.getBean(HelloBean.class);
+        assertThat(bean).isNotNull();
+    }
+
+    @Test
+    void selectorConfig() {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SelectorConfig.class);
+        HelloBean bean = context.getBean(HelloBean.class);
+        assertThat(bean).isNotNull();
+    }
+
+    @Configuration
+    @Import(HelloConfig.class)
+    public static class StaticConfig {}
+
+    @Configuration
+    @Import(HelloImportSelector.class)
+    public static class SelectorConfig {}
+}
