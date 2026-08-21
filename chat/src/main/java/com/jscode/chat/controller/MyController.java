e8104755525a9d3cc0c4aaf054625423a1ecb5b8
Choi, Minwoo <chaiminwoo0223@gmail.com>
2026-08-21 23:06:40 +0900

    Streaming으로 Flux 형태로 응답받기

--- chat/src/main/java/com/jscode/chat/controller/MyController.java
+++ chat/src/main/java/com/jscode/chat/controller/MyController.java
@@ -1,8 +1,10 @@
 package com.jscode.chat.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class MyController {
@@ -18,5 +20,13 @@ public class MyController {
                 .user(userPrompt)
                 .call()
                 .content(); // 받아온 응답 중 메타데이터는 버리고, 순수 content만 추출!
+    }
+
+    @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> stream(String userPrompt) {
+        return this.chatClient.prompt()
+                .user(userPrompt)
+                .stream()
+                .content();
     }
 }
